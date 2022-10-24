68b4e7fa94860d594d0424c016290de49eaabebd
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-10-23 23:23:04 -0500

    many code rewrites

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
 const html = require('./src/generateHtml');
-let teamMembers = []
+let teamMembers = [];
 
 function initTeam () {
 inquirer
@@ -15,7 +18,7 @@ inquirer
     {
         type: 'input',
         name: 'id',
-        message: `Enter the team member's badge ID.`
+        message: `Enter the team member's employee ID.`
     },
     {
         type: 'input',
@@ -34,60 +37,142 @@ inquirer
         name: 'officeNumber',
         message: `What is the manager's office number?`,
         when: (answers) => answers.role === 'Manager'
-    },
+},
+
     {
         type: 'input',
         name: 'github',
-        message: `What is the engineer's user name?`,
-        when: (answers) => answers.role === 'Engineer'
+        message: `What is the engineer's GitHub user name?`,
+        when: (answers) => answers.role === 'Engineer' 
     },
+
     {
         type: 'input',
         name: 'school',
         message: `In what school is the intern enrolled?`,
-        when: (answers) => answers.role === 'Intern'
-    }
-])
-.then((answers) => {
-    if (answers.role === "Manager") {
-      const manager = new Manager(answers.name, answers.id, answers.email, answers.role, answers.officeNumber);
-      teamMembers.push(manager);
-    }
-    if (answers.role === "Engineer") {
-      const engineer = new Engineer(answers.name, answers.id, answers.email, answers.role, answers.github);
-      teamMembers.push(engineer);
-    }
-    if (answers.role === "Intern") {
-      const intern = new Intern(answers.name, answers.id, answers.email, answers.role, answers.school);
-      teamMembers.push(intern);
-    }
-});
-addMore();
-}
-
-function addMore () {
-    inquirer
-    .prompt ([
-        {
+        when: (answers) => answers.role === 'Intern' 
+  }, 
+    {
         type: "confirm",
         name: "addMore",
         message: "Would you like to add another team member?",
         validate: (confirm) => {
-            if (confirm) {
-                return initTeam();
-              } else {
-                teamMembers.forEach((teamMembers) => {
-                  console.log(teamMembers);
-                  return teamMembers;
-                });
-            fs.writeFile('index.html', html(response), 'utf-8', (err) =>
+            if (confirm === 'Yes') {
+                 (answers) => {
+                        if (answers.role === "Manager") {
+                          const manager = new Manager(answers.name, answers.id, answers.email, answers.role, answers.officeNumber);
+                          teamMembers.push(manager);
+                        }
+                        else if (answers.role === "Engineer") {
+                          const engineer = new Engineer(answers.name, answers.id, answers.email, answers.role, answers.github);
+                          teamMembers.push(engineer);
+                        }
+                        else if (answers.role === "Intern") {
+                          const intern = new Intern(answers.name, answers.id, answers.email, answers.role, answers.school);
+                          teamMembers.push(intern);
+                        }
+                }
+                initTeam();
+              } 
+            else {
+            fs.writeFile('index.html', html(teamMembers), 'utf-8', (err) =>
             err ? console.error(err) : console.log('Populating your webpage.'), console.log(response))
+            console.log(teamMembers);
+            console.log("Populating your team webpage...");
+             }
             }
-             console.log("Populating your team webpage...");
-         
-               }
-            }])
-              }
+    }
+])
+    // .then((answers) => {
+    //     if (answers.role === "Manager") {
+    //       const manager = new Manager(answers.name, answers.id, answers.email, answers.role, answers.officeNumber);
+    //       teamMembers.push(manager);
+    //     }
+    //     else if (answers.role === "Engineer") {
+    //       const engineer = new Engineer(answers.name, answers.id, answers.email, answers.role, answers.github);
+    //       teamMembers.push(engineer);
+    //     }
+    //     else if (answers.role === "Intern") {
+    //       const intern = new Intern(answers.name, answers.id, answers.email, answers.role, answers.school);
+    //       teamMembers.push(intern);
+    //     }
+}
+initTeam();
+
+//     .then((response) =>
+//    fs.writeFile('index.html', generateHtml(response), 'utf-8', (err) =>
+//     err ? console.error(err) : console.log('Populating your webpage.'), console.log(response)))
+//     }   
+//   })}
+
+
+
+// function to choose what data to render based on the role chosen
+
+
+// function renderExtraInfo(data) {
+//     let role = data.role[0];
+//     let extraInfo = ``;
+  
+//     if (role === 'Engineer') {
+//        extraInfo = `<p>GitHub: <a href='https://github.com/${github}'>${github}</p>`;
+       
+//     };
+//     if (role === 'Manager') {
+//         extraInfo = `<p>Room #: ${officeNumber}</p>`;
+//     };
+//     if (role === 'Intern') {
+//         extraInfo = `<p>School: ${school}</p>`;
+//     };
+//     console.log(extraInfo);    
+//     return extraInfo;
+     
+//   }
+
+// teamMembers.forEach(populateCard(data));
+    
+// function populateCard(data) {
+//     return `
+//         <div class="col">
+//             <div class="card bg-light shadow mx-auto" style="width: 18rem;">
+//                 <div class="card-header bg-warning bg-gradient">
+//                   <p class="fs-3 lh-md fw-semibold text-white">${data.name}</p>
+//                   <p class="fs-4 fw-semibold text-white">${data.role}</p>
+//                 </div>
+//                 <ul class="list-group list-group-flush">
+//                   <li class="list-group-item"><p>ID: ${data.id}</p></li>
+//                   <li class="list-group-item"><p>Email: <a href="mailto:${data.email}">${data.email}</p></a></li>
+                  
+//                 </ul>
+//               </div>
+//           </div>`
+//   }
+
+// function generateHtml(data) {
+//     return `
+// <!doctype html>
+// <html lang="en">
+//   <head>
+//     <meta charset="utf-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1">
+//     <title>My Team</title>
+//     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+//   </head>
+//   <body>
+//     <div class="container-fluid py-5 bg-success bg-gradient">
+//         <h1 class="text-center text-white">My Team</h1>
+//     </div>
+//     <div class="container-fluid">
+//         <div class="row mt-5">
+// ${(populateCard(data))}
+//             <li class="list-group-item">${renderExtraInfo(data)}</li>
+//             </div>
+//         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+//     </body>
+// </html>
+// `;      }
+
+
 
 
 // .then((response) =>
@@ -108,4 +193,3 @@ function addMore () {
 
 
 // Function call to initialize app
-initTeam();
--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,41 +1,64 @@
 // function to choose what data to render based on the role chosen
-function renderExtraInfo(data) {
-    let role = data.role[0];
-    let extraInfo = ``;
-  
+function renderHtmlCards (...teamMembers) {
+    let role = teamMembers.role[0];
+    let htmlCards = ``
+
     if (role === 'Engineer') {
-       extraInfo = `<p>GitHub: <a href='https://github.com/${github}'>${github}</p>`;
-       
+       engineerCard = `
+       <div class="col">
+           <div class="card bg-light shadow mx-auto" style="width: 18rem;">
+               <div class="card-header bg-warning bg-gradient">
+                 <p class="fs-3 lh-md fw-semibold text-white">${engineer.name}</p>
+                 <p class="fs-4 fw-semibold text-white"><i class="fa-solid fa-laptop-code"></i>${engineer.role}</p>
+               </div>
+               <ul class="list-group list-group-flush">
+                 <li class="list-group-item"><p>ID: ${engineer.id}</p></li>
+                 <li class="list-group-item"><p>Email: <a href="mailto:${engineer.email}">${engineer.email}</p></a></li>
+                 <li class="list-group-item"><p>GitHub: <a href='https://github.com/${engineer.github}'>${engineer.github}</p></li>
+               </ul>
+            </div>
+      </div>`;
+      htmlCards.push(engineerCard);
     };
     if (role === 'Manager') {
-        extraInfo = `<p>Room #: ${officeNumber}</p>`;
+        managerCard =  `
+        <div class="col">
+        <div class="card bg-light shadow mx-auto" style="width: 18rem;">
+            <div class="card-header bg-warning bg-gradient">
+              <p class="fs-3 lh-md fw-semibold text-white">${manager.name}</p>
+              <p class="fs-4 fw-semibold text-white"><i class="fa-solid fa-laptop-code"></i>${manager.role}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><p>ID: ${manager.id}</p></li>
+              <li class="list-group-item"><p>Email: <a href="mailto:${manager.email}">${manager.email}</p></a></li>
+              <li class="list-group-item"><p>Room #: ${manager.officeNumber}</p></li>
+            </ul>
+         </div>
+   </div>`;
+   htmlCards.push(managerCard)
     };
     if (role === 'Intern') {
-        extraInfo = `<p>School: ${school}</p>`;
-    };
-    console.log(extraInfo);    
-    return extraInfo;
-     
-  }
-
-  function populateCard(data) {
-    return `
+        internCard = `
         <div class="col">
-            <div class="card bg-light shadow mx-auto" style="width: 18rem;">
-                <div class="card-header bg-warning bg-gradient">
-                  <p class="fs-3 lh-md fw-semibold text-white">${data.name}</p>
-                  <p class="fs-4 fw-semibold text-white">${data.role}</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item"><p>ID: ${data.id}</p></li>
-                  <li class="list-group-item"><p>Email: <a href="mailto:${data.email}">${data.email}</p></a></li>
-                  
-                </ul>
-              </div>
-          </div>`
-  }
-  
-function generateHtml(data) {
+        <div class="card bg-light shadow mx-auto" style="width: 18rem;">
+            <div class="card-header bg-warning bg-gradient">
+              <p class="fs-3 lh-md fw-semibold text-white">${intern.name}</p>
+              <p class="fs-4 fw-semibold text-white"><i class="fa-solid fa-laptop-code"></i>${intern.role}</p>
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><p>ID: ${intern.id}</p></li>
+              <li class="list-group-item"><p>Email: <a href="mailto:${intern.email}">${intern.email}</p></a></li>
+              <li class="list-group-item"><p>School: ${intern.school}</p>/li>
+            </ul>
+         </div>
+   </div>`;
+   htmlCards.push(internCard)
+    };  
+    return htmlCards;  
+}
+
+ 
+function generateHtml(...teamMembers) {
     return `
 <!doctype html>
 <html lang="en">
@@ -44,21 +67,42 @@ function generateHtml(data) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>My Team</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  </head>
+    <script src="https://kit.fontawesome.com/0b4245b6ab.js" crossorigin="anonymous"></script>
+    </head>
   <body>
     <div class="container-fluid py-5 bg-success bg-gradient">
         <h1 class="text-center text-white">My Team</h1>
     </div>
     <div class="container-fluid">
         <div class="row mt-5">
-${(populateCard(data))}
-            <li class="list-group-item">${renderExtraInfo(data)}</li>
+${renderHtmlCards(...teamMembers)}
+            <li class="list-group-item"></li>
             </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </body>
 </html>
-`;      }
+`;     
+}
 
 
 
 module.exports = generateHtml
+
+
+// function populateCard() {
+  //     return `
+  //         <div class="col">
+  //             <div class="card bg-light shadow mx-auto" style="width: 18rem;">
+  //                 <div class="card-header bg-warning bg-gradient">
+  //                   <p class="fs-3 lh-md fw-semibold text-white">${teamMembers.name}</p>
+  //                   <p class="fs-4 fw-semibold text-white">${teamMembers.role}</p>
+  //                 </div>
+  //                 <ul class="list-group list-group-flush">
+  //                   <li class="list-group-item"><p>ID: ${teamMembers.id}</p></li>
+  //                   <li class="list-group-item"><p>Email: <a href="mailto:${teamMembers.email}">${teamMembers.email}</p></a></li>
+  //                   ${renderExtraInfo(...teamMembers)}
+  //                 </ul>
+  //               </div>
+  //           </div>`
+  //   }
+  
